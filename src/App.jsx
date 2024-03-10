c81d3ffc4045cb7d6f57ef050cb05a1d266cdcb7
Alan-Nixon <alannixon2520@gmail.com>
2024-03-10 16:24:59 +0530

    after resolved the session

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Register from './assets/Register/Register'
-import Cookies from 'js-cookie'
 import Login from './assets/Login/Login'
 import AdminLogin from './assets/adminLogin/AdminLogin'
+import Home from './assets/Home/Home'
+import AdminHome from './assets/AdminHome/AdminHome'
+import { jwtDecode } from 'jwt-decode';
+
+
 function App() {
+
   const [user, setUser] = useState(null)
-  const VerifyUser = async () => {
-    const token = Cookies.get('token')
-    if (!token) {
-      return setUser(false)
+  const [session, setSession] = useState(false)
+
+  const IsUserExist = ({ ifSession, NotSession }) => {
+    const token = JSON.parse(localStorage.getItem('userToken'))
+    if (token) {
+      console.log(token);
+      let decodedToken = jwtDecode(token);
+      let currentDate = new Date();
+      if (decodedToken.exp * 1000 > currentDate.getTime()) { setSession(true) }
     }
-    const response = await fetch('http://localhost:3000/verify', {
-      method: "POST",
-      headers: { 'Authorization': `Bearer ${token}` },
-    })
-
-    const { data } = await response.json()
+    console.log(ifSession, NotSession);
+    return session ? ifSession : NotSession
   }
-
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/register' element={<Register userState={{ user, setUser }} />} />
-          <Route path='/login' element={<Login userState={{ user, setUser }} />} />
-          <Route path='/officials/login' element={<AdminLogin userState={{ user, setUser }} />} />
+          <Route path='/' element={<IsUserExist ifSession={<Home userState={{ user, setUser }} />} NotSession={<Navigate to='/login' />} />} />
+          <Route path='/register' element={<IsUserExist ifSession={<Navigate to='/' />} NotSession={<Register userState={{ user, setUser }} />} />} />
+          <Route path='/login' element={<IsUserExist ifSession={<Navigate to='/' />} NotSession={<Login userState={{ user, setUser }} />} />} />
+
+          <Route path='/officials/login' element={<IsUserExist ifSession={<Navigate to='/officials' />} NotSession={<AdminLogin userState={{ user, setUser }} />} />} />
+          <Route path='/officials' element={<IsUserExist ifSession={<AdminHome />} NotSession={<Navigate to='/officials/login' />} />} />
         </Routes>
       </Router>
+
     </>
   )
 }
